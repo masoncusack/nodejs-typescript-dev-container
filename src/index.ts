2d62d7a07c392bede78d45b76e72ba52608d40e1
Mason Cusack <macusack>
2019-10-04 11:41:28 +0100

    Dockerfile produces ubuntu image with Node and npm

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import * as http from "http";
 import template from "./lib";
 
-console.log(`entrypoint: hello ${template.hello}`)
+// tslint:disable-next-line: no-console
+console.log(`Hello ${template.hello}! Try echo server: \n` +
+    "curl -d 'echo test' -X POST http://localhost:8080");
+
+http.createServer((req, res) => {
+    req.pipe(res);
+}).listen(8080);
